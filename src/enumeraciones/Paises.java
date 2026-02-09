466388d23e061e6ce458d9ec224c2ffc8efcf911
guillermogb <guillermogb@profe208b.informatica.cifpaviles.pa>
2026-02-09 12:36:22 +0100

    Se añaden paises.

---
+++ src/enumeraciones/Paises.java
@@ -0,0 +1,13 @@
+package enumeraciones;
+
+public enum Paises {
+	
+	ESPANA(0034);
+	//TODO: resto de prefijos por meter.
+	
+	public int prefijo;
+	
+	Paises(int prefijo) {
+		this.prefijo=prefijo;
+	}
+}
